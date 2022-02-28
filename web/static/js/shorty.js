4b3ddfebdf13bcde063106160ac716fca607961b
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2022-02-28 16:05:16 -0500

    Learning things

--- web/static/js/shorty.js
+++ web/static/js/shorty.js
@@ -1,12 +1,18 @@
 function getShorty(){
-    let url = document.getElementById(`url`).value
 
-    let text = $.ajax({
-        type:`GET`,
-        url:`localhost:4040/get-shorty`,
+    console.log("Calling this function")
+
+    let url = document.getElementById('url').value
+
+    $.ajax({
+        type:'POST',
+        url:'localhost:4040/get-shorty',
         data:url,
+        headers: {'Access-Control-Allow-Origin' : 'localhost:4040'}
     })
 
-    document.getElementById('shorty').innerHTML = text
+    document.getElementById('shorty').innerHTML = "text"
+
+    document.getElementById('url').value = ""
 
 }
