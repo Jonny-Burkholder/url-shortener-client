da098f913699bddaf80edfcd9e54f6fc38ad6ace
Jonny-Burkholder <48441498+Jonny-Burkholder@users.noreply.github.com>
2022-03-01 12:28:42 -0500

    Update shorty.js

--- web/static/js/shorty.js
+++ web/static/js/shorty.js
@@ -1,7 +1,5 @@
 function getShorty(){
 
-    console.log("Calling this function");
-
     let input = document.getElementById('url').value;
 
     $.ajax({
@@ -10,20 +8,19 @@ function getShorty(){
         url:'http://localhost:4040/get-shorty',
         dataType: 'json',
         data:JSON.stringify(input),
-        headers: {'Access-Control-Allow-Origin' : '*'},
         contentType: 'application/json; charset=urf-8',
     })
     .done(function(data, status, jqxhr){
         console.log("Great success!");
         console.log(data, status, jqxhr);
+        document.getElementById('shorty').innerHTML = data;
     })
 
     .fail(function(jqxhr, status, error){
         console.log("Failure");
         console.log(jqxhr, status, error);
+        document.getElementById('shorty').innerHTML = error;
     })
-
-    document.getElementById('shorty').innerHTML = "text";
 
     document.getElementById('url').value = "";
 
